d7c945eac2a1dbb7f9a99dec87fbff2a3cd45767
teo1264 <s.gubitoso@gmail.com>
2025-05-21 11:22:33 -0300

    Update __init__.py
    resolvendo dependencias para o render

--- utils/__init__.py
+++ utils/__init__.py
@@ -5,4 +5,34 @@
 Pacote de utilitários para o CCB Alerta Bot
 """
 
-# Este arquivo permite que a pasta utils seja importada como um pacote
+# Importar todas as funções necessárias para disponibilizar no namespace do pacote
+from .utils import (
+    verificar_formato_cadastro,
+    extrair_dados_cadastro,
+    fazer_backup_planilha,
+    verificar_cadastro_existente,
+    salvar_cadastro,
+    buscar_usuario_por_id,
+    buscar_usuarios_por_codigo,
+    buscar_todos_usuarios,
+    atualizar_cadastro,
+    remover_cadastro,
+    criar_pasta_temporaria,
+    remover_pasta_temporaria,
+    registrar_consentimento_lgpd,
+    verificar_consentimento_lgpd,
+    remover_consentimento_lgpd
+)
+
+# Importar funções do banco de dados diretamente para o namespace do utils
+from utils.database import (
+    verificar_admin,
+    adicionar_admin,
+    listar_admins,
+    salvar_responsavel,
+    buscar_responsavel_por_id,
+    buscar_responsaveis_por_codigo,
+    listar_todos_responsaveis,
+    remover_responsavel,
+    remover_responsavel_especifico
+)
